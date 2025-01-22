ccf030949aef2c9d6f29e8579111a37928765bf0
sswz666 <536799205@qq.com>
2025-01-22 11:42:37 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import re
+from os import system
+from flask import Flask
+from requests import *
+
+clashheaders = {
+    'User-Agent': 'ClashforWindows/0.20.39'
+}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'
+}
+
+def gethtml():
+    try:
+        html = get("https://github.site/tolinkshare2/tolinkshare2.github.io/blob/main/README.md", headers=headers,verify=False, proxies={})
+        if html.status_code != 200:
+            html = get("https://bgithub.xyz/tolinkshare2/tolinkshare2.github.io/blob/main/README.md", headers=headers,verify=False, proxies={})
+        if html.status_code != 200:
+            html = get("https://github.com/tolinkshare2/tolinkshare2.github.io/blob/main/README.md", headers=headers,verify=False, proxies={})
+        return html.text
+    except Exception as e:
+        print(f"请求失败: {e}")
+        system("exit")
+        return ""
+
+def novpn():
+    if 'HTTP_PROXY' in os.environ:
+        del os.environ['HTTP_PROXY']
+    if 'HTTPS_PROXY' in os.environ:
+        del os.environ['HTTPS_PROXY']
+
+def getclash(url):
+    try:
+        novpn()
+        response = get(url, headers=clashheaders,verify=False,proxies = {'http': None,'https': None})
+        return response.text
+    except RequestException as e:
+        print(f"请求失败: {e}")
+        return ""
+
+def findhttp(html):
+    pattern = r'<code>(.*?)</code>'
+    url=re.findall(pattern, html, re.DOTALL)
+    url = (url[0])
+    url=url[:-1]
+    url = url.replace("https://", "http://")
+    return url
+
+def startflask():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def got():
+        txt = main()
+        return txt
+
+    app.run("127.0.0.1", 2025)
+def main():
+    novpn()
+    html=gethtml()
+    url=findhttp(html)
+    clash=getclash(url)
+    return clash
+
+startflask()
+
